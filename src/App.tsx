c90d5556bee353496b1df3d78de72c414b1c6da6
SebaDL <jsdilauro@gmail.com>
2025-01-13 16:51:11 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import VotingResults from "./components/VotingResults"
+import { Typography } from "@mui/material";
 
 const App: React.FC = () => {
   return (
     <div style={{ padding: "20px" }}>
       <VotingResults />
+      <Typography gutterBottom align="center" >
+        Made by Protocol Squad to avoid opening and comparing 20 tabs {'<'}3
+      </Typography>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import darkTheme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline /> {/* Aplica estilos básicos como fondo oscuro */}
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

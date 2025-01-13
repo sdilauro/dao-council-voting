673f293467da79360ce22eb11c0a385a718cbacd
SebaDL <jsdilauro@gmail.com>
2025-01-13 16:51:02 -0300

    dark and message

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#121212",
+      paper: "#1e1e1e",
+    },
+    text: {
+      primary: "#ffffff",
+      secondary: "#b0b0b0",
+    },
+  },
+});
+
+export default darkTheme;
